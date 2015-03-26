8ce7e5af577892245d367cfca70483b1e39971ab
cindraling <nicolas.monnier44@gmail.com>
2015-03-26 15:10:20 +0100

    ajout de proto + .c
    création du .c et écriture des premières fonctions

--- Projet/listeDeplacement.h
+++ Projet/listeDeplacement.h
@@ -48,7 +48,7 @@ int last(ListDeplacement * l);
 * \param[in] ListeDeplacement
 * \return    1 si on est en dehors de la liste, 0 sinon
 */
-int outOfList(ListDeplacement* * l);
+int outOfList(ListDeplacement * l);
 
 /**
 * \brief      Positionne le curseur au début de la liste
@@ -62,8 +62,44 @@ void setOnFirst(ListDeplacement * l);
 */
 void setOnLast(ListDeplacement * l);
 
-//123456cbcb
+/**
+* \brief      Positionne le curseur à la position suivante
+* \param[in/out] ListeDeplacement
+*/
+void next(ListDeplacement * l);
 
+/**
+* \brief      Positionne le curseur à la position précédente
+* \param[in/out] ListeDeplacement
+*/
+void previous(ListDeplacement *l);
 
+/**
+* \brief      indique où se trouve le curseur
+* \param[in/out] ListeDeplacement
+* \return     la posittion du curseur dans le fichier texte
+*/
+int getCurrent(ListDeplacement * l);
+
+/**
+* \brief     ecrit dans le fichier à l'emplacement courantdu curseur 
+*			 toutes les informations du déplacement effectué
+* \param[in/out] ListeDeplacement
+*/
+void printFileCurrentList(ListDeplacement * l);
+
+/**
+* \brief     Création du fichier contenant toutes les informations relatives
+*			 aux déplacements pendant une partie
+* \return    NULL si erreur de création
+*/
+int createHistoryFile(void);
+
+/**
+* \brief     Sauvegarde dans un fichier texte le déplacement qui vient d'être effectué 
+*			 puis décale le curseur pour se préparer pour la pochaine sauvegarde
+* \return    Si la sauvegarde s'est bien passé
+*/
+int saveDeplacement(ListDeplacement *l);
 
 #endif
