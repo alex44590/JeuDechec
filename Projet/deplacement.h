ced5f7967958990a5b68948ab22b7e91e4846496
cindraling <nicolas.monnier44@gmail.com>
2015-06-04 22:33:31 +0200

    Doxy deplacement.h + listDeplacement.h

--- Projet/deplacement.h
+++ Projet/deplacement.h
@@ -6,6 +6,17 @@
 #include "plateauDeJeu.h"
 #include "deplacementPossible.h"
 
+
+/**
+* \fn void retourArriere(ListDeplacement *l, PlateauDeJeu *p, MenuDroite * m, SDL_Renderer* contexte, Couleur* couleurAJouer, ContexteRoque* c)
+* \brief     retourne au à l'état du déplacement précédent, met à jour le tableau de pièce 
+* \param[in/out] ListeDeplacement
+* \param[in/out] PlateauDeJeu 
+* \param[in/out] MenuDroite
+* \param[in/out] SDL_Renderer
+* \param[in/out] CouleurAJouer
+* \param[in/out] ContexteRoque
+*/
 void retourArriere(ListDeplacement *l, PlateauDeJeu *p, MenuDroite * m, SDL_Renderer* contexte, Couleur* couleurAJouer, ContexteRoque* c);
 
 #endif
--- Projet/listeDeplacement.h
+++ Projet/listeDeplacement.h
@@ -26,18 +26,22 @@ typedef struct {
 }ListDeplacement;
 
 /**
+* \fn Deplacement * newDeplacement(Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement, Deplacement* next, Deplacement* previous)
 * \brief      Créer un nouveau noeud de déplacement
 * \param[in] tous les paramètres du déplacement effectué
 * \return ListDeplacement : Le nouveau noeud créé
 */
 Deplacement * newDeplacement(Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement, Deplacement* next, Deplacement* previous);
 
-/**IDPiece piece, IDCase depart, IDCase arrivee, int numeroDeplacement, Timer heure
+/**
+* \fn ListDeplacement* initListDeplacement()
 * \brief      initialise à NULL ListeDeplacement
 * \param[in/out] ListeDeplacement : paramaètres à mettre à NULL
 */
 ListDeplacement* initListDeplacement();
+
 /**
+* \fn int empty(ListDeplacement * l)
 * \brief      Vérifie si la liste des déplacement est vide
 * \param[in] ListeDeplacement
 * \return   1 si la liste est vide, 0 sinon
@@ -45,6 +49,7 @@ ListDeplacement* initListDeplacement();
 int empty(ListDeplacement * l);
 
 /**
+* \fn int first(ListDeplacement * l)
 * \brief      Vérifie si on se trouve au début de la liste des déplacement
 * \param[in] ListeDeplacement
 * \return    1 si on est au début de la liste, 0 sinon
@@ -52,6 +57,7 @@ int empty(ListDeplacement * l);
 int first(ListDeplacement * l);
 
 /**
+* \fn int last(ListDeplacement * l)
 * \brief      Vérifie si on est à la fin de la liste
 * \param[in] ListeDeplacement
 * \return    1 si on est à la fin de la liste, 0 sinon
@@ -59,6 +65,7 @@ int first(ListDeplacement * l);
 int last(ListDeplacement * l);
 
 /**
+* \fn int outOfList(ListDeplacement * l)
 * \brief      Vérifie si on est en dehors de la liste
 * \param[in] ListeDeplacement
 * \return    1 si on est en dehors de la liste, 0 sinon
@@ -66,42 +73,68 @@ int last(ListDeplacement * l);
 int outOfList(ListDeplacement * l);
 
 /**
+* \fn void setOnFirst(ListDeplacement * l)
 * \brief      Positionne le curseur au début de la liste
 * \param[in/out] ListeDeplacement
 */
 void setOnFirst(ListDeplacement * l);
 
 /**
+* \fn void setOnLast(ListDeplacement * l)
 * \brief      Positionne le curseur à la fin de la liste
 * \param[in/out] ListeDeplacement
 */
 void setOnLast(ListDeplacement * l);
 
 /**
+* \fn int insertFirst(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement)
 * \brief      insert un noeud en premiere position
 * \param[in/out] ListeDeplacement + éléments de déplacements
 * \return 0 si le déplacement ne s'est pas bien passé 1 sinon
 */
 int insertFirst(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement);
 
+/**
+* \fn int insertLast(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement)
+* \brief      insert un noeud en dernière position
+* \param[in/out] ListeDeplacement + éléments de déplacements
+* \return 0 si le déplacement ne s'est pas bien passé 1 sinon
+*/
 int insertLast(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement);
+
+/**
+* \fn int insertAfterCurrentBL(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement);
+* \brief      insert un noeud après le noeud courant
+* \param[in/out] ListeDeplacement + éléments de déplacements
+* \return 0 si le déplacement ne s'est pas bien passé 1 sinon
+*/
 int insertAfterCurrentBL(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement);
+
+/**
+* \fn int createNewDeplacement(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement)
+* \brief      Créer un nouveau noeud Deplacement
+* \param[in/out] ListeDeplacement + éléments de déplacements
+* \return 0 si le déplacement ne s'est pas bien passé 1 sinon
+*/
 int createNewDeplacement(ListDeplacement *l, Piece *piece, IDCase depart, IDCase arrivee, int numeroDeplacement);
 
 
 /**
+* \fn void next(ListDeplacement * l)
 * \brief      Positionne le curseur à la position suivante
 * \param[in/out] ListeDeplacement
 */
 void next(ListDeplacement * l);
 
 /**
+* \fn void previous(ListDeplacement *l)
 * \brief      Positionne le curseur à la position précédente
 * \param[in/out] ListeDeplacement
 */
 void previous(ListDeplacement *l);
 
 /**
+* \fn int getCurrent(ListDeplacement * l)
 * \brief      indique où se trouve le curseur
 * \param[in/out] ListeDeplacement
 * \return     la posittion du curseur dans le fichier texte
@@ -109,6 +142,7 @@ void previous(ListDeplacement *l);
 int getCurrent(ListDeplacement * l);
 
 /**
+* \fn int printFileCurrentDeplacement(ListDeplacement * l, int *posCurseur)
 * \brief     ecrit dans le fichier à l'emplacement courantdu curseur
 *			 toutes les informations du déplacement effectué
 * \param[in/out] ListeDeplacement
@@ -117,6 +151,7 @@ int getCurrent(ListDeplacement * l);
 int printFileCurrentDeplacement(ListDeplacement * l, int *posCurseur);
 
 /**
+* \fn int createHistoryFile(void)
 * \brief     Création du fichier contenant toutes les informations relatives
 *			 aux déplacements pendant une partie
 * \return    NULL si erreur de création
@@ -124,19 +159,54 @@ int printFileCurrentDeplacement(ListDeplacement * l, int *posCurseur);
 int createHistoryFile(void);
 
 /**
+* \fn int saveDeplacement(ListDeplacement *l)
 * \brief     Sauvegarde dans un fichier texte le déplacement qui vient d'être effectué
 *			 puis décale le curseur pour se préparer pour la pochaine sauvegarde
 * \return    Si la sauvegarde s'est bien passé
 */
 int saveDeplacement(ListDeplacement *l);
 
+
+
+/**
+* \fn void printPiece(ListDeplacement * l, int *posCurseur)
+* \brief     ecrit dans le fichier à l'emplacement courantdu curseur
+*			 L'ID de la pièce
+* \param[in/out] ListeDeplacement
+* \param[in/out] posCurseur  La position du curseur en lecture/écriture
+*/
 void printPiece(ListDeplacement * l, int *posCurseur);
 
+/**
+* \fn void printDepart(ListDeplacement * l, int *posCurseur)
+* \brief     ecrit dans le fichier à l'emplacement courantdu curseur
+*			 L'ID de la case de départ du déplacement
+* \param[in/out] ListeDeplacement
+* \param[in/out] posCurseur  La position du curseur en lecture/écriture
+*/
 void printDepart(ListDeplacement * l, int *posCurseur);
+
+/**
+* \fn void printArrivee(ListDeplacement * l, int *posCurseur)
+* \brief     ecrit dans le fichier à l'emplacement courantdu curseur
+*			 L'ID de la case de d'arrivé du déplacement
+* \param[in/out] ListeDeplacement
+* \param[in/out] posCurseur  La position du curseur en lecture/écriture
+*/
 void printArrivee(ListDeplacement * l, int *posCurseur);
 
-
+/**
+* \fn void deleteCurrent(ListDeplacement* l)
+* \brief     Supprime le déplacement courant
+* \param[in/out] ListeDeplacement
+*/
 void deleteCurrent(ListDeplacement* l);
+
+/**
+* \fn void deleteLast(ListDeplacement* l)
+* \brief     Supprime le dernier déplacement
+* \param[in/out] ListeDeplacement
+*/
 void deleteLast(ListDeplacement* l);
 
 #endif
