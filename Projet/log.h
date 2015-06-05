d31f9b474d386c90c0270591561e8dfc4e94e4c7
cindraling <nicolas.monnier44@gmail.com>
2015-06-05 17:11:24 +0200

    Doxy log.h piece.h

--- Projet/log.h
+++ Projet/log.h
@@ -10,12 +10,20 @@ typedef enum { INFO, AVERTISSEMENT, ERREUR }LogType;
 
 
 /*Fonction servant simplement à créer un nouveau fichier de log VIDE*/
+/**
+* \fn void logInit()
+* \brief   Créer un nouveau fichier log vide
+*/
 void logInit();
 
 //Fct permettant d'écrire un message de type info, avertissement ou erreur dans le fichier de log
+/**
+* \fn void logPrint(LogType typeMessage, char* message)
+* \brief   Permet d'écrire unu message d'info, avertissement ou erreur dans le fichier log
+* \param[in] LogType typeMessage,
+* \param[in] char* message
+*/
 void logPrint(LogType typeMessage, char* message);
-
-
 
 
 #endif // !LOG_H
--- Projet/piece.h
+++ Projet/piece.h
@@ -9,13 +9,74 @@
 
 extern int numeroDeplacement;
 
+/**
+* \fn Piece* creerPiece(TypePiece type, Couleur couleur, char numPiece)
+* \brief   Créer et initialise une pièce du jeu 
+* \param[in] TypePiece type
+* \param[in] Couleur couleur
+* \param[in] char numPiece
+* \return un pointeur vers la pièce créée 
+*/
 Piece* creerPiece(TypePiece type, Couleur couleur, char numPiece);
+
+/**
+* \fn void bougerPiece(Piece* p, Piece* tabPiece[8][8], Lettre colonneArrivee, Lettre ligneArrivee, ListDeplacement* l)
+* \brief   Déplace une pièce sur l'échiquier
+* \param[in] Piece* p
+* \param[in] Piece* tabPiece[8][8]
+* \param[in] Lettre colonneArrivee
+* \param[in] LettreligneArrivee
+* \param[in] ListDeplacement* l
+*/
 void bougerPiece(Piece* p, Piece* tabPiece[8][8], Lettre colonneArrivee, Lettre ligneArrivee, ListDeplacement* l);
+
+/**
+* \fn void afficherPiece(Piece* p, SDL_Renderer* contexte)
+* \brief   Affiche une piece sur sa position sur l'échiquier
+* \param[in] Piece* p
+* \param[in] SDL_Renderer* contexte
+*/
 void afficherPiece(Piece* p, SDL_Renderer* contexte);
+
+/**
+* \fn void afficherAllPiece(Piece* tabPiece[8][8], SDL_Renderer* contexte)
+* \brief   Affiche toutes les pieces sur l'échiquier
+* \param[in] Piece* tabPiece[8][8]
+* \param[in] SDL_Renderer* contexte
+*/
 void afficherAllPiece(Piece* tabPiece[8][8], SDL_Renderer* contexte);
+
+/**
+* \fn void initAllPiece(Piece* tabPiece[8][8], ListDeplacement* l)
+* \brief   Créer toutes les pièces de départ d'une partie d'échec
+* \param[in] Piece* tabPiece[8][8]
+* \param[in] ListDeplacement* l
+*/
 void initAllPiece(Piece* tabPiece[8][8], ListDeplacement* l);
+
+/**
+* \fn void mettreEnSurbillancePiece(Piece* p, SDL_Renderer* contexte)
+* \brief   Met en surbrillance la piece sélectionnée
+* \param[in] Piece* p
+* \param[in] SDL_Renderer* contexte
+*/
 void mettreEnSurbillancePiece(Piece* p, SDL_Renderer* contexte);
+
+/**
+* \fn void supprimerSurbillancePiece(Piece* p, SDL_Renderer* contexte)
+* \brief   Supprime la surbrillance de la pièce s'électionné
+* \param[out] Piece* p
+* \param[in] SDL_Renderer* contexte
+*/
 void supprimerSurbillancePiece(Piece* p, SDL_Renderer* contexte);
+
+/**
+* \fn void mangerPiece(Piece* p, Piece* tabPiece[8][8], ListDeplacement* l)
+* \brief   mange une pièce adverse
+* \param[out] Piece* p
+* \param[in] Piece* tabPiece[8][8]
+* \param[in] ListDeplacement* l
+*/
 void mangerPiece(Piece* p, Piece* tabPiece[8][8], ListDeplacement* l);
 
 #endif
